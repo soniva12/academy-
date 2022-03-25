95435da86e7d0d250f3ef4cb4cf270caa733b647
soniva12 <102335948+soniva12@users.noreply.github.com>
2022-03-25 03:16:37 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# soniva12" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/soniva12/soniva12.git
+git push -u origin main
